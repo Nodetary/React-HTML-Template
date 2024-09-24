65f8bb0f5eb49837f1d785aa104f04907e258ec4
nodetary.eth <106583148+Nodetary@users.noreply.github.com>
2024-09-24 01:26:18 -0500

    Initial commit: React template setup

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to Modern React Template</h1>
+      <p>This is the home page of our React application.</p>
+    </div>
+  );
+}
+
+export default Home;
